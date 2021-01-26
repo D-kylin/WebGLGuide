33d3562c610ce74a21802079acd9f79ff705ec91
D-kylin <2017kylin@gmail.com>
2021-01-26 17:46:21 +0800

    Chapter 2

--- 02/2-4.js
+++ 02/2-4.js
@@ -9,8 +9,10 @@ ready(() => {
     }
   `
   const FSHADER_SOURCE = `
+    precision mediump float;
+    uniform vec4 u_FragColor;
     void main() {
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+      gl_FragColor = u_FragColor;
     }
   `
 
@@ -21,15 +23,16 @@ ready(() => {
     
     initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)
     
+    // 获取
     const a_Position = gl.getAttribLocation(gl.program, 'a_Position')
     const a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize')
+    const u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor')
 
     gl.vertexAttrib1f(a_PointSize, 10.0)
     
     // 在 canvas 上注册鼠标点击事件
     canvas.onmousedown = function(e) {
-      console.log(e)
-      click(e, gl, canvas, a_Position)
+      click(e, gl, canvas, a_Position, u_FragColor)
     }
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0)
@@ -38,13 +41,14 @@ ready(() => {
 
 
   const g_points = []
-  function click(event, gl, canvas, a_Position) {
+  const g_colors = []
+  function click(event, gl, canvas, a_Position, u_FragColor) {
     let x = event.clientX
     let y = event.clientY
     const rect = event.target.getBoundingClientRect()
 
     /**
-     * 注意：这里的x、y值计算与书本有差异，经过实践，发现是书本印刷有误
+     * 注意：这里的 x、y 值计算与书本有差异，经过实践，发现是书本印刷有误
      * 
      * x = ((x - rect.left) - canvas.height / 2) / (canvas.height / 2)
      * y = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2)
@@ -54,12 +58,25 @@ ready(() => {
 
     // 添加点坐标
     g_points.push([x, y])
+    
+    // 添加颜色
+    if (x >= 0.0 && y >= 0.0) {
+      g_colors.push([1.0, 0.0, 0.0, 1.0])
+    } else if (x < 0.0 && y < 0.0) {
+      g_colors.push([0.0, 1.0, 0.0, 1.0])
+    } else {
+      g_colors.push([1.0, 1.0, 1.0, 1.0])
+    }
 
     gl.clear(gl.COLOR_BUFFER_BIT)
-    for (let point of g_points) {
-      const [x, y] = point
+    for (let i = 0; i < g_points.length; i++) {
+      const [x, y] = g_points[i]
+      const [r, g, b, a] = g_colors[i]
       // 设置每个点对应的矢量
       gl.vertexAttrib3f(a_Position, x, y, 0.0)
+
+      // 设置每个点的颜色
+      gl.uniform4f(u_FragColor, r, g, b, a)
 
       gl.drawArrays(gl.POINTS, 0, 1)
     }
